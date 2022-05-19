0c51750ea9b1c717b4079c83d68bab98ade87c27
QGZ2S8H <koelblMaximilian@Johndeere.com>
2022-05-19 16:12:58 +0200

    Try Run

--- git-release.sh
+++ git-release.sh
@@ -1,28 +1,47 @@
-#!/bin/bash
+#!/bin/sh
 
-version=$1
-text=$2
+token=$1
+versionfile=$(<version.json)
+version=$(sed 's/.*"version": "\(.*\)".*/\1/;t;d' version.json)
+
 branch=$(git rev-parse --abbrev-ref HEAD)
 repo_full_name=$(git config --get remote.origin.url | sed 's/.*:\/\/github.com\///;s/.git$//')
-token=$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
+#last_tag=$(git describe --abbrev=0 --tags)
 
-#https://github.com/maxl-13/Workflow_Bachelorarbeit.git
-
-#maxl-13/Workflow_Bachelorarbeit
-
-generate_post_data()
+generate_post_data_release()
 {
   cat <<EOF
 {
   "tag_name": "$version",
   "target_commitish": "$branch",
   "name": "$version",
-  "body": "$text",
+  "body": "value",
   "draft": false,
-  "prerelease": false
+  "prerelease": false,
+  "generate_release_notes": true
 }
 EOF
 }
 
-echo "Create release $version for repo: $repo_full_name branch: $branch"
-curl -H 'Authorization: token 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' --data "$(generate_post_data)" "https://api.github.com/repos/$repo_full_name/releases?access_token=$token"
+
+AUTH="Authorization: token $token"
+GH_API="https://api.github.com/repos/maxl-13/Workflow_Bachelorarbeit/releases"
+GH_REPO="$GH_API/repos/$repo_full_name"
+GH_TAGS="$GH_REPO/releases/latest"
+
+echo "$AUTH"
+
+curl -o /dev/null -sH "$AUTH" $GH_REPO || { echo "Error: Invalid repo, token or network issue!";  exit 1; }
+
+response=$(curl -X POST -H "$AUTH" -H "Accept: application/vnd.github.v3+json" --data "$(generate_post_data)" "https://api.github.com/repos/$repo_full_name/releases")
+
+eval $(echo "$response" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
+[ "$id" ] || { echo "Error: Failed to get release id for tag: $tag"; echo "$response" | awk 'length($0)<100' >&2; exit 1; }
+
+FILES="$@"
+for file in $FILES
+do
+  if test -f "$file"; then
+    curl -H "$AUTH" -H "Accept: application/vnd.github.v3+json" -H "Content-Type: application/octet-stream" --data-binary @"$file" "https://uploads.github.com/repos/MPS/NETStandard-lib/releases/$id/assets?name=$(basename $file)"
+  fi
+done
